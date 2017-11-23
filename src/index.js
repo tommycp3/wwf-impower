e1fdde5570d3e945260ccbb92dfdf7342bd26ffc
Thomas Miller <thomasmiller@Thomass-MacBook-Pro.local>
2017-11-24 10:53:32 +1100

    installed bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
